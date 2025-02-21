d0d047d2906aaf50cbad5be8cebff904c6ad1bfd
Jeffrey Chang <78696780+zwjc@users.noreply.github.com>
2025-02-21 13:08:45 +0100

    Feature/18 coverage improvement get buffered image (#27)
    * Update pom.xml for tests
    
    * bug: update pom
    
    * feat: Unit tests for TiffImageParser
    
    * chore: improve readability
    
    * bug: remove vscode push
    
    Solves #18

--- src/test/java/org/apache/commons/imaging/formats/tiff/TiffImageParserTest.java
+++ src/test/java/org/apache/commons/imaging/formats/tiff/TiffImageParserTest.java
@@ -4,7 +4,8 @@
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * the License.
+ * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -15,27 +16,90 @@
  * limitations under the License.
  */
 
-package org.apache.commons.imaging.formats.tiff;
+ package org.apache.commons.imaging.formats.tiff;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+ import static org.junit.jupiter.api.Assertions.assertThrows;
+ import static org.mockito.Mockito.mock;
+ import static org.mockito.Mockito.when;
+ 
+ import java.io.File;
+ import java.nio.ByteOrder;
+ 
+ import org.apache.commons.imaging.ImagingException;
+ import org.apache.commons.imaging.ImagingFormatException;
+ import org.apache.commons.imaging.bytesource.ByteSource;
+ import org.apache.commons.imaging.formats.tiff.constants.TiffTagConstants;
+ import org.junit.jupiter.api.Test;
+ 
+ /**
+  * Tests for TiffImageParser, including:
+  * 1) A regression test for Google oss-fuzz issue 53669.
+  * 2) Three tests that exercise branches 3, 4, and 12 in getBufferedImage().
+  */
+ public class TiffImageParserTest {
+ 
+     /**
+      * Regression test for Google oss-fuzz issue 53669.
+      * This test should remain as you originally had it.
+      */
+     @Test
+     public void testOssFuzzIssue53669() {
+         assertThrows(ImagingFormatException.class,
+             () -> new TiffImageParser().getBufferedImage(
+                 ByteSource.file(new File(
+                     "src/test/resources/images/tiff/oss-fuzz-53669/"
+                     + "clusterfuzz-testcase-minimized-ImagingTiffFuzzer-5965016805539840.tiff")),
+                 null));
+     }
+ 
+     @Test
+     public void testZeroWidth() throws Exception {
 
-import java.io.File;
+         TiffImagingParameters mockedParams = mock(TiffImagingParameters.class);
+         when(mockedParams.isSubImageSet()).thenReturn(true);
+         when(mockedParams.getSubImageX()).thenReturn(0);
+         when(mockedParams.getSubImageY()).thenReturn(0);
+         when(mockedParams.getSubImageWidth()).thenReturn(0);
+         when(mockedParams.getSubImageHeight()).thenReturn(100);
+ 
+         TiffDirectory directory = mock(TiffDirectory.class);
+         when(directory.findField(TiffTagConstants.TIFF_TAG_COMPRESSION)).thenReturn(null);
+         when(directory.getSingleFieldValue(TiffTagConstants.TIFF_TAG_IMAGE_WIDTH)).thenReturn(100);
+         when(directory.getSingleFieldValue(TiffTagConstants.TIFF_TAG_IMAGE_LENGTH)).thenReturn(100);
+         when(directory.getFieldValue(TiffTagConstants.TIFF_TAG_PHOTOMETRIC_INTERPRETATION))
+             .thenReturn((short)2);
+ 
 
-import org.apache.commons.imaging.ImagingFormatException;
-import org.apache.commons.imaging.bytesource.ByteSource;
-import org.junit.jupiter.api.Test;
+         assertThrows(ImagingException.class,
+             () -> new TiffImageParser().getBufferedImage(directory, ByteOrder.BIG_ENDIAN, mockedParams));
+     }
 
-/**
- * Tests Google oss-fuzz issue 53669.
- */
-public class TiffImageParserTest {
 
-    @Test
-    public void testOssFuzzIssue53669() {
-        assertThrows(ImagingFormatException.class,
-                () -> new TiffImageParser().getBufferedImage(
-                        ByteSource.file(new File(
-                                "src/test/resources/images/tiff/oss-fuzz-53669/clusterfuzz-testcase-minimized-ImagingTiffFuzzer-5965016805539840.tiff")),
-                        null));
-    }
-}
+     @Test
+     public void testMisMatch() throws Exception {
+
+         TiffImagingParameters params = new TiffImagingParameters();
+ 
+         TiffDirectory directory = mock(TiffDirectory.class);
+         when(directory.findField(TiffTagConstants.TIFF_TAG_COMPRESSION)).thenReturn(null);
+         when(directory.getSingleFieldValue(TiffTagConstants.TIFF_TAG_IMAGE_WIDTH)).thenReturn(100);
+         when(directory.getSingleFieldValue(TiffTagConstants.TIFF_TAG_IMAGE_LENGTH)).thenReturn(100);
+         when(directory.getFieldValue(TiffTagConstants.TIFF_TAG_PHOTOMETRIC_INTERPRETATION))
+             .thenReturn((short)2);
+ 
+         TiffField samplesPerPixelField = mock(TiffField.class);
+         when(samplesPerPixelField.getIntValue()).thenReturn(1);
+         when(directory.findField(TiffTagConstants.TIFF_TAG_SAMPLES_PER_PIXEL))
+             .thenReturn(samplesPerPixelField);
+ 
+         TiffField bitsPerSampleField = mock(TiffField.class);
+         when(bitsPerSampleField.getIntArrayValue()).thenReturn(new int[] {8, 8});
+         when(bitsPerSampleField.getIntValueOrArraySum()).thenReturn(16);
+         when(directory.findField(TiffTagConstants.TIFF_TAG_BITS_PER_SAMPLE))
+             .thenReturn(bitsPerSampleField);
+ 
+         assertThrows(ImagingException.class,
+             () -> new TiffImageParser().getBufferedImage(directory, ByteOrder.BIG_ENDIAN, params));
+     }
+ 
+ }
