b41e2258202aac390b70d59e7800f915b93c4f54
dazhijiong <dazhi2001@163.com>
2025-02-21 15:06:06 +0100

    bugfix: add test cases

--- src/test/java/org/apache/commons/imaging/formats/tiff/TiffReadImageInfoTest.java
+++ src/test/java/org/apache/commons/imaging/formats/tiff/TiffReadImageInfoTest.java
@@ -38,7 +38,7 @@ public class TiffReadImageInfoTest extends TiffBaseTest {
     static final String[][] testSet = { { "1/matthew2.tif", "Color Type", "Black and White" },
             { "7/Oregon Scientific DS6639 - DSC_0307 - small - CMYK.tiff", "Color Type", "CMYK" }, { "10/Imaging247.TIFF", "Uses Palette", "true" },
             { "12/TransparencyTestStripAssociated.tif", "Is Transparent", "true" }, { "14/TestJpegStrips.tiff", "Color Type", "YCbCr" },
-            { "12/TransparencyTestStripAssociated.tif", "Is Transparent", "true" }, { "14/TestJpegStrips.tiff", "Color Type", "YCbCr" },{"3/1pagefax.tif","Compression Algorithm","ccitt 1d"},//coverage_improve_1
+            {"3/1pagefax.tif","Compression Algorithm","ccitt 1d"},//coverage_improve_1
             {"3/Oregon Scientific DS6639 - DSC_0307 - small CCITT T.6.tiff","Compression Algorithm","ccitt group 4"}//coverage_improve_2
     };
 
