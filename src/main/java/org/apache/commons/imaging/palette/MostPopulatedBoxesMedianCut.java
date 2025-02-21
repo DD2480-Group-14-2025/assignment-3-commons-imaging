1d940b4c2e70ceb59ece4dd7f8dc937fec3581ee
Yolanda WANG <ywangpb@connect.ust.hk>
2025-02-21 10:14:01 +0100

    chore: refactor performNextMedianCut, decreasing its CCN to 3

--- src/main/java/org/apache/commons/imaging/palette/MostPopulatedBoxesMedianCut.java
+++ src/main/java/org/apache/commons/imaging/palette/MostPopulatedBoxesMedianCut.java
@@ -25,6 +25,21 @@ public class MostPopulatedBoxesMedianCut implements MedianCut {
 
     @Override
     public boolean performNextMedianCut(final List<ColorGroup> colorGroups, final boolean ignoreAlpha) throws ImagingException {
+        ColorGroup colorGroup = findMaxPointsColorGroup(colorGroups);
+        if (colorGroup == null) {
+            return false;
+        }
+
+        BestCutResult bestCutResult = calculateBestMedianCut(colorGroup, ignoreAlpha);
+        if (bestCutResult.bestColorComponent == null) {
+            return false;
+        }
+
+        applyBestCut(colorGroups, colorGroup, bestCutResult, ignoreAlpha);
+        return true;
+    }
+
+    private ColorGroup findMaxPointsColorGroup(final List<ColorGroup> colorGroups) {
         int maxPoints = 0;
         ColorGroup colorGroup = null;
         for (final ColorGroup group : colorGroups) {
@@ -33,92 +48,110 @@ public class MostPopulatedBoxesMedianCut implements MedianCut {
                 maxPoints = group.totalPoints;
             }
         }
-        if (colorGroup == null) {
-            return false;
-        }
+        return colorGroup;
+    }
 
-        final List<ColorCount> colorCounts = colorGroup.getColorCounts();
-
+    private BestCutResult calculateBestMedianCut(final ColorGroup colorGroup, final boolean ignoreAlpha) throws ImagingException {
         double bestScore = Double.MAX_VALUE;
         ColorComponent bestColorComponent = null;
         int bestMedianIndex = -1;
+        List<ColorCount> colorCounts = colorGroup.getColorCounts();
+
         for (final ColorComponent colorComponent : ColorComponent.values()) {
             if (ignoreAlpha && colorComponent == ColorComponent.ALPHA) {
                 continue;
             }
             colorCounts.sort(new ColorCountComparator(colorComponent));
-            final int countHalf = (int) Math.round((double) colorGroup.totalPoints / 2);
-            int oldCount = 0;
-            int newCount = 0;
-            int medianIndex;
-            for (medianIndex = 0; medianIndex < colorCounts.size(); medianIndex++) {
-                final ColorCount colorCount = colorCounts.get(medianIndex);
+            int medianIndex = findMedianIndex(colorCounts, colorGroup.totalPoints);
 
-                newCount += colorCount.count;
-
-                if (newCount >= countHalf) {
-                    break;
-                }
-                oldCount = newCount;
-            }
-            if (medianIndex == colorCounts.size() - 1) {
-                medianIndex--;
-            } else if (medianIndex > 0) {
-                final int newDiff = Math.abs(newCount - countHalf);
-                final int oldDiff = Math.abs(countHalf - oldCount);
-                if (oldDiff < newDiff) {
-                    medianIndex--;
-                }
-            }
-
-            final List<ColorCount> lowerColors = new ArrayList<>(colorCounts.subList(0, medianIndex + 1));
-            final List<ColorCount> upperColors = new ArrayList<>(colorCounts.subList(medianIndex + 1, colorCounts.size()));
+            List<ColorCount> lowerColors = colorCounts.subList(0, medianIndex + 1);
+            List<ColorCount> upperColors = colorCounts.subList(medianIndex + 1, colorCounts.size());
             if (lowerColors.isEmpty() || upperColors.isEmpty()) {
                 continue;
             }
-            final ColorGroup lowerGroup = new ColorGroup(lowerColors, ignoreAlpha);
-            final ColorGroup upperGroup = new ColorGroup(upperColors, ignoreAlpha);
-            final int diff = Math.abs(lowerGroup.totalPoints - upperGroup.totalPoints);
-            final double score = diff / (double) Math.max(lowerGroup.totalPoints, upperGroup.totalPoints);
+
+            double score = calculateScore(lowerColors, upperColors, ignoreAlpha);
             if (score < bestScore) {
                 bestScore = score;
                 bestColorComponent = colorComponent;
                 bestMedianIndex = medianIndex;
             }
         }
+        return new BestCutResult(bestScore, bestColorComponent, bestMedianIndex);
+    }
 
-        if (bestColorComponent == null) {
-            return false;
+    private int findMedianIndex(final List<ColorCount> colorCounts, final int totalPoints) {
+        final int countHalf = (int) Math.round((double) totalPoints / 2);
+        int oldCount = 0;
+        int newCount = 0;
+        int medianIndex;
+        for (medianIndex = 0; medianIndex < colorCounts.size(); medianIndex++) {
+            final ColorCount colorCount = colorCounts.get(medianIndex);
+            newCount += colorCount.count;
+            if (newCount >= countHalf) {
+                break;
+            }
+            oldCount = newCount;
         }
+        if (medianIndex == colorCounts.size() - 1) {
+            medianIndex--;
+        } else if (medianIndex > 0) {
+            final int newDiff = Math.abs(newCount - countHalf);
+            final int oldDiff = Math.abs(countHalf - oldCount);
+            if (oldDiff < newDiff) {
+                medianIndex--;
+            }
+        }
+        return medianIndex;
+    }
 
-        colorCounts.sort(new ColorCountComparator(bestColorComponent));
-        final List<ColorCount> lowerColors = new ArrayList<>(colorCounts.subList(0, bestMedianIndex + 1));
-        final List<ColorCount> upperColors = new ArrayList<>(colorCounts.subList(bestMedianIndex + 1, colorCounts.size()));
-        final ColorGroup lowerGroup = new ColorGroup(lowerColors, ignoreAlpha);
-        final ColorGroup upperGroup = new ColorGroup(upperColors, ignoreAlpha);
+    private double calculateScore(final List<ColorCount> lowerColors, final List<ColorCount> upperColors, boolean ignoreAlpha) throws ImagingException {
+        ColorGroup lowerGroup = new ColorGroup(lowerColors, ignoreAlpha);
+        ColorGroup upperGroup = new ColorGroup(upperColors, ignoreAlpha);
+        int diff = Math.abs(lowerGroup.totalPoints - upperGroup.totalPoints);
+        return diff / (double) Math.max(lowerGroup.totalPoints, upperGroup.totalPoints);
+    }
+
+    private void applyBestCut(final List<ColorGroup> colorGroups, final ColorGroup colorGroup, final BestCutResult bestCutResult, final boolean ignoreAlpha) throws ImagingException {
+        List<ColorCount> colorCounts = colorGroup.getColorCounts();
+        colorCounts.sort(new ColorCountComparator(bestCutResult.bestColorComponent));
+        List<ColorCount> lowerColors = new ArrayList<>(colorCounts.subList(0, bestCutResult.bestMedianIndex + 1));
+        List<ColorCount> upperColors = new ArrayList<>(colorCounts.subList(bestCutResult.bestMedianIndex + 1, colorCounts.size()));
+        ColorGroup lowerGroup = new ColorGroup(lowerColors, ignoreAlpha);
+        ColorGroup upperGroup = new ColorGroup(upperColors, ignoreAlpha);
         colorGroups.remove(colorGroup);
         colorGroups.add(lowerGroup);
         colorGroups.add(upperGroup);
 
-        final ColorCount medianValue = colorCounts.get(bestMedianIndex);
-        final int limit;
+        ColorCount medianValue = colorCounts.get(bestCutResult.bestMedianIndex);
+        int limit = determineLimit(bestCutResult.bestColorComponent, medianValue);
+        colorGroup.cut = new ColorGroupCut(lowerGroup, upperGroup, bestCutResult.bestColorComponent, limit);
+    }
+
+    private int determineLimit(ColorComponent bestColorComponent, ColorCount medianValue) {
         switch (bestColorComponent) {
-        case ALPHA:
-            limit = medianValue.alpha;
-            break;
-        case RED:
-            limit = medianValue.red;
-            break;
-        case GREEN:
-            limit = medianValue.green;
-            break;
-        case BLUE:
-            limit = medianValue.blue;
-            break;
-        default:
-            throw new IllegalArgumentException("Bad mode: " + bestColorComponent);
+            case ALPHA:
+                return medianValue.alpha;
+            case RED:
+                return medianValue.red;
+            case GREEN:
+                return medianValue.green;
+            case BLUE:
+                return medianValue.blue;
+            default:
+                throw new IllegalArgumentException("Bad mode: " + bestColorComponent);
         }
-        colorGroup.cut = new ColorGroupCut(lowerGroup, upperGroup, bestColorComponent, limit);
-        return true;
+    }
+
+    private static class BestCutResult {
+        double bestScore;
+        ColorComponent bestColorComponent;
+        int bestMedianIndex;
+
+        BestCutResult(double bestScore, ColorComponent bestColorComponent, int bestMedianIndex) {
+            this.bestScore = bestScore;
+            this.bestColorComponent = bestColorComponent;
+            this.bestMedianIndex = bestMedianIndex;
+        }
     }
 }
